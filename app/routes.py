79b1428e7ffdf1fe195800fdf45785824830d32c
Emma <emma@example.com>
2022-03-24 17:33:15 -0700

    v1

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
-from app import myapp_obj
+from app import myobj
 from app.forms import LoginForm
-from flask import render_template, request
+from flask import render_template, request, flash, redirect
 
 
-@myapp_obj.route("/", methods = ['GET', 'POST'])
+name = {'name':'Carlos'}
+city_names = [{'city':'Paris'}, {'city':'New york'}, {'city': 'Tokyo'}, {'city':'Livermore'}]
+
+@myobj.route("/", methods = ['GET', 'POST'])
 def home():
-	name = {'name':'Carlos'}
-	city_names = [{'city':'Paris'}, {'city':'New york'}, {'city': 'Tokyo'}, {'city':'Livermore'}]
 	form = LoginForm()
-	if form.is_submitted():
-		result = request.form
-		return render_template ('not_google.html', username = name, city_names = city_names, form = form, result = result)
-	return render_template('not_google.html', username = name, city_names = city_names, form = form)
+	if form.validate_on_submit():
+		flash(form.user_input.data)
+		return redirect('/')
+	return render_template('home.html', username = name, city_names = city_names, form = form)
