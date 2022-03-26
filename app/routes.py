801a46a994813bc48b58bd78476141d95d6d19ce
Emma <emma@example.com>
2022-03-26 00:22:53 -0700

    finally, nope

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,10 @@ from app import LoginForm
 from flask import render_template, request, flash, redirect
 
 
-name = {'name':'Carlos'}
+name = Carlos
 city_names = [{'city':'Paris'}, {'city':'New york'}, {'city': 'Tokyo'}, {'city':'Livermore'}]
 
-@myobj.route("/", methods = ['GET', 'POST'])
+@myobj.route("/", methods = 'POST')
 def home():
 	form = LoginForm()
 	if form.validate_on_submit():
